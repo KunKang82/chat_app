4a9494d29116af4aef7b7183230556aa99554182
KunKang82 <kunzsayz@hotmail.com>
2023-07-02 21:16:26 -0400

    5.4

--- App.js
+++ App.js
@@ -8,30 +8,50 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //import firebase and firestore
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+
+// NetInfo for detecting a Network Connection
+import { useNetInfo } from '@react-native-community/netinfo';
+
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 // The app’s main Chat component that renders the chat UI
 const App = () => {
-// Web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAh216Ud4G42LCeZQSX1eYy-LtMHLbWbvQ",
-  authDomain: "chatapp-be8d0.firebaseapp.com",
-  projectId: "chatapp-be8d0",
-  storageBucket: "chatapp-be8d0.appspot.com",
-  messagingSenderId: "579769078008",
-  appId: "1:579769078008:web:b70d7ea19f4d019370eb0d"
-};
+  // Defines a new state that represents the network connectivity status
+  const connectionStatus = useNetInfo();
+  
+  // Displays an alert popup if connection is lost and load offline messages from cache
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAh216Ud4G42LCeZQSX1eYy-LtMHLbWbvQ",
+    authDomain: "chatapp-be8d0.firebaseapp.com",
+    projectId: "chatapp-be8d0",
+    storageBucket: "chatapp-be8d0.appspot.com",
+    messagingSenderId: "579769078008",
+    appId: "1:579769078008:web:b70d7ea19f4d019370eb0d"
+  };
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
 
   return (
+    // NavigationContainer navigates between start and chat screens
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Start"
@@ -43,8 +63,8 @@ const db = getFirestore(app);
         <Stack.Screen
           name="Chat"
         >
-          {props => <Chat db={db} {...props} />}
-        </Stack.Screen>        
+          {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Chat.js
+++ components/Chat.js
@@ -1,41 +1,84 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import { collection, query, orderBy, addDoc, onSnapshot } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
+
+  // Destructre the parameters form the route object
   const { name, color, userID } = route.params;
+
+  // Set the initial state for messages
   const [messages, setMessages] = useState([]);
 
+  // Need to declare unsubMessages variable outside the useEffect() callback function, but inside the compnent so that
+  // you can unsubscribe and disable the old onSnapshot listener before you lose any reference to it. This is because
+  // re-initializing any kind of listener doesn’t override and remove the old listener, it only removes the reference
+  // to that old listener, while keeping it alive somewhere in the memory without a way of reaching it, resulting in a memory leak.
+  let unsubMessages;
 
+  // if there's a connection: fetch messages from firebase, if not: load from Async storage (loadCachedMessages)
   useEffect(() => {
     //Set screen title according to give name from prop
     navigation.setOptions({ title: name })
 
-    // Create stream with database to read messages
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, docs => {
-      let newMessages = [];
-      docs.forEach(doc => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis())
+    // Check if connected to the internet
+    if (isConnected === true) {
+      // Unregister current onSnapshot() listener to avoid registering multiple
+      // listeners when useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      // Create stream with database to read messages
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      // Subscribe to real-teim updates using onSnapshot
+      unsubMessages = onSnapshot(q, docs => {
+        let newMessages = [];
+        // process each document and create a new message object
+        docs.forEach(doc => {
+          newMessages.push({
+            id: doc.id, // Set unique _id value for each message
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis())
+          });
         });
+        // Cache the messages and update the state
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
+      // Load cached messages if not connected
+    } else loadCachedMessages();
 
     // Clean up code
     return () => {
       if (unsubMessages) unsubMessages();
     }
-  }, []);
+  }, [isConnected]); // Allows the component to call the callback of useEffect whenever the isConnected prop's value changes
 
+  // Load messages from AsyncStorage cache and stores messages to localStorage (asyncStorage)
+  const loadCachedMessages = async () => {
+    // || [] will assign an empty array to cachedMessages in case AsyncStorage.getItem("chat") fails when the chat item has 
+    // been set yet in AsyncStorage. Known as "logical OR assignment operator" in jS
+    const cachedMessages = await AsyncStorage.getItem("chat") || [];
+    setMessages(JSON.parse(cachedMessages));
+  }
+
+  // same logic as when using localStorage, and "try-catch" is an error handler to prevent the app from crashing in case
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem('chat', JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // Handle sending a new message
   const onSend = (newMessages) => {
     addDoc(collection(db, "messages"), newMessages[0])
   }
 
+  // Customize the appearance of the message bubble
   const renderBubble = (props) => {
     return <Bubble
       {...props}
@@ -50,15 +93,25 @@ const Chat = ({ route, navigation, db }) => {
     />
   }
 
+  // Render the input toolbar if connected
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  }
+
+  // Render the Chat component
   return (
     //Set background color according to given prop color from start screen
     <View style={[styles.container, { backgroundColor: color }]}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar} //function that returns <InputToolbar {...props}/> if there’s a connection, otherwise, it returns a null
         onSend={messages => onSend(messages)}
-        user={{ _id: userID, 
-                name: name }}     
+        user={{
+          _id: userID,
+          name: name
+        }}
       />
       {/* fix for blocked view on android */}
       {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
